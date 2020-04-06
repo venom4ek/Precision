c4c7f97cb0e777a76b1f6352512163f8b64e90dc
venomek <venom4ek@gmail.com>
2020-04-07 01:31:24 +0300

    ïnitial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double regularBonus = 0.30;
+        double specialBonus = 0.60;
+        double totalBonus = regularBonus + specialBonus;
+        System.out.println(totalBonus);
+    }
+}
